b8c42c4a5687f5083d6bfad59f57599961231494
shivam-18 <shivam.goel2511@gmail.com>
2021-05-08 03:09:07 +0530

    shivam

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Prod = require('../models/prod');
-
-
-class example_base_prod {
+var sel_id_cnt=2;
+var del_id_cnt=2;
+/*class example_base_prod {
     constructor(){
         this.id = 1;
         this.name = "BG";
@@ -11,9 +11,26 @@ class example_base_prod {
         this.rating = 5;
         this.category = "Randi";
     }
+}*/
+
+class base_prod{
+    constructor(id,name, image, cost, seller, rating, quantity, description){
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.cost = cost;
+        this.seller = seller;
+        this.rating = rating.toFixed(2);
+        this.quantity = quantity;
+        this.description = description;
+    }
 }
 
-class example_buyer {
+function prod_p(rec,seller){
+    return new base_prod(rec['id'],rec['title'],rec['img'],rec['price'], seller, rec['rating'],rec['quantity'],rec['description']);
+}
+
+/*class example_buyer {
     constructor(name, address, contact, mail){
         this.id = 1;
         this.name = "Dolly";
@@ -22,9 +39,24 @@ class example_buyer {
         this.mail = "dololol@gmail.com";
         this.balance = 210983;
     }
+}*/
+
+class buyer {
+    constructor(id,name, address, contact, mail,balance){
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+        this.mail = mail;
+        this.balance = balance;
+    }
 }
 
-class example_seller {
+function prod_b(rec){
+    return new buyer(rec['id'],rec['name'],rec['address'],rec['contact'],rec['mail'],rec['money']);
+}
+
+/*class example_seller {
     constructor(name, address, contact, mail){
         this.id = 1;
         this.name = "Dolly_sell";
@@ -32,9 +64,23 @@ class example_seller {
         this.contact = "0008881212";
         this.mail = "seldololol@gmail.com";
     }
+}*/
+
+class seller {
+    constructor(id,name, address, contact, mail){
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+        this.mail = mail;
+    }
 }
 
-class example_delagents {
+function prod_s(rec){
+    return new seller(rec['id'],rec['name'],rec['address'],rec['contact'],rec['mail']);
+}
+
+/*class example_delagents {
     constructor(name, address, contact, mail){
         this.id = 1;
         this.name = "Dolly_del";
@@ -44,49 +90,88 @@ class example_delagents {
         this.num_delivered = 40;
         this.num_active = 3;
     }
+}*/
+
+class delagents {
+    constructor(id,name, contact, mail,n1,n2){
+        this.id = id;
+        this.name = name;
+        this.contact = contact;
+        this.mail = mail;
+        this.num_delivered = n1;
+        this.num_active = n2;
+    }
+}
+
+function prod_d(rec){
+    return new delagents(rec['id'],rec['name'],rec['contact'],rec['mail'],rec['delivered'],rec['active']);
 }
 
 exports.get_browse = (req,res,next) => {
-    p = new example_base_prod();
-    arr = [p,p,p,p,p,p];
-    res.render('admin/browse', {
-        pageTitle: 'Browse',
-        path: '/admin/browse',
-        editing: false,
-        prods: arr
+    db_session
+    .run("match (p:product)-[:prod_sell]->(s:seller) return p,s.name",{})
+    .then(function(result){
+        arr=[]
+        for (i=0;i<result.records.length;i++){
+            arr.push(prod_p(result.records[i].get('p').properties,result.records[i].get('s.name')));
+        }
+        res.render('admin/browse', {
+            pageTitle: 'Browse',
+            path: '/admin/browse',
+            editing: false,
+            prods: arr
+        });
     });
 };
 
 exports.get_buyers = (req,res,next) => {
-    d = new example_buyer()
-    arr = [d,d,d,d,d,d,d]
-    res.render('admin/buyers', {
-        pageTitle: 'Buyers',
-        path: '/admin/buyers',
-        editing: false,
-        buyers: arr
+    db_session
+    .run("match (b:buyer) return b",{})
+    .then(function(result){
+        arr=[]
+        for (i=0;i<result.records.length;i++){
+            arr.push(prod_b(result.records[i].get('b').properties));
+        }
+        res.render('admin/buyers', {
+            pageTitle: 'Buyers',
+            path: '/admin/buyers',
+            editing: false,
+            buyers: arr
+        });
     });
 };
 
 exports.get_sellers = (req,res,next) => {
-    d = new example_seller()
-    arr = [d,d,d,d,d,d,d]
-    res.render('admin/sellers', {
-        pageTitle: 'Sellers',
-        path: '/admin/sellers',
-        editing: false,
-        sellers: arr
+    db_session
+    .run("match (s:seller) return s",{})
+    .then(function(result){
+        arr=[]
+        for (i=0;i<result.records.length;i++){
+            arr.push(prod_s(result.records[i].get('s').properties));
+        }
+        res.render('admin/sellers', {
+            pageTitle: 'Sellers',
+            path: '/admin/sellers',
+            editing: false,
+            sellers: arr
+        });
     });
 };
 
 exports.get_delagents = (req,res,next) => {
-    d = new example_delagents()
-    arr = [d,d,d,d,d,d,d]
-    res.render('admin/delagents', {
-        pageTitle: 'Delivery Agents',
-        path: '/admin/delagents',
-        editing: false,
-        delagents: arr
+    db_session
+    .run("match (d:del_personnel) return d",{})
+    .then(function(result){
+        arr=[]
+        for (i=0;i<result.records.length;i++){
+            arr.push(prod_d(result.records[i].get('d').properties));
+        }
+        res.render('admin/delagents', {
+            pageTitle: 'Delivery Agents',
+            path: '/admin/delagents',
+            editing: false,
+            delagents: arr
+        });
     });
 };
 
@@ -115,10 +200,37 @@ exports.get_about = (req,res,next) => {
 };
 
 exports.post_add_seller = (req,res,next) => {
+    var name = req.body.name;
+	var uname = req.body.uname;
+    var psw = req.body.psw;
+    var address = req.body.address;
+    var contact = req.body.contact;
+    var mail = req.body.mail;
+
+    db_session
+        .run("merge (s:seller{id:$id,name:$na,username:$un,password:$pw,address:$ad,contact:$co,mail:$ma})",
+            {id:sel_id_cnt,na:name,un:uname,pw:psw,ad:address,co:contact,ma:mail})
+        .then(function(result){
+            sel_id_cnt+=1;
+            res.redirect('/admin/sellers');
+        });
     return
 };
 
 exports.post_add_delagent = (req,res,next) => {
+    var name = req.body.name;
+	var uname = req.body.uname;
+    var psw = req.body.psw;
+    var contact = req.body.contact;
+    var mail = req.body.mail;
+
+    db_session
+        .run("merge (d:del_personnel{id:$id,name:$na,username:$un,password:$pw,contact:$co,mail:$ma,delivered:$n1,active:$n2})",
+            {id:del_id_cnt,na:name,un:uname,pw:psw,co:contact,ma:mail,n1:0,n2:0})
+        .then(function(result){
+            del_id_cnt+=1;
+            res.redirect('/admin/delagents');
+        });
     return
 };
 
